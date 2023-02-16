6095b8866d5831d7194d72d88e0dabca0002b8ca
DrashtiBhatt1801 <drashti.bhatt1801@gmail.com>
2023-02-16 09:28:58 -0500

    first commit

---
+++ src/main/java/com/example/JPAUsingRest/JpaUsingRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.JPAUsingRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaUsingRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaUsingRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JPAUsingRest/JpaUsingRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JPAUsingRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaUsingRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
